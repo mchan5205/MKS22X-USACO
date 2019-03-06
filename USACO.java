a94bbf17d5869482cac0bbb21f45bdc49177a29f
Michael Chan <mchan10@stuy.edu>
2019-03-06 10:49:49 -0500

    added board creation

--- USACO.java
+++ USACO.java
@@ -2,7 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class USACO{
-  public static int bronze(String filename){
-
+  public static int bronze(String filename) throws FileNotFoundException{
+    File f = new File(filename);
+    Scanner s = new Scanner(f);
+    int r = s.nextInt();
+    int c = s.nextInt();
+    int e = s.nextInt();
+    int n = s.nextInt();
+    int[][] field = new int[r][c];
+    for (int x = 0; x < r; x++){
+      s.nextLine();
+      for (int y = 0; y < c; y++){
+        field[x][y] = s.nextInt();
+      }
+    }
+  }
+  public static void main(String[] args) throws FileNotFoundException{
+    bronze("test.txt");
   }
 }
