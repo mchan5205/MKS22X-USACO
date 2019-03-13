a13cf10548df4e59903947e396de049f7eb9b212
Michael Chan <mchan10@stuy.edu>
2019-03-13 00:26:43 -0400

    fixed bronze

--- USACO.java
+++ USACO.java
@@ -15,27 +15,24 @@ public class USACO{
       for (int y = 0; y < c; y++){
         field[x][y] = s.nextInt();
       }
-    }/*
-    File b = new File(filename);
-    Scanner g = new Scanner(b);
-    */
+    }
     for (int i = 0; i < n; i++){
       s.nextLine();
       int rs = s.nextInt() - 1;
       int cs = s.nextInt() - 1;
       int ds = s.nextInt();
       int max = field[rs][cs];
-      for (int q = rs; q < 3; q ++){
-        for (int w = cs; w < 3; w++){
-          if (field[q][w] > max){
-            max = field[q][w];
+      for (int q = 0; q < 3; q++){
+        for (int w = 0; w < 3; w++){
+          if (field[rs + q][cs + w] > max){
+            max = field[rs + q][cs + w];
           }
         }
       }
       for (int p = 0; p < 3; p++){
         for (int o = 0; o < 3; o++){
-          if (field[p][o] > max - ds){
-            field[p][o] = max - ds;
+          if (field[rs + p][cs + o] >= max - ds){
+            field[rs + p][cs + o] = max - ds;
           }
         }
       }
@@ -103,6 +100,6 @@ public class USACO{
     return movecount[s.nextInt() - 1][s.nextInt() - 1];
   }
   public static void main(String[] args) throws FileNotFoundException{
-    System.out.println(silver("ctravel.2.in"));
+    System.out.println(bronze("makelake.3.in"));
   }
 }
